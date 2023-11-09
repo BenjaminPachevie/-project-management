0edac5f45bffe322040cfd6356a6388f1587e831
Benjamin Pachevie <b.pachevie@gmail.com>
2023-11-09 18:46:23 +0100

    adding new project + errorModal"

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
   });
 
   function handleStartAddProject() {
@@ -15,21 +15,50 @@ function App() {
       return {
         ...prevState,
         selectedProjectId: null,
-      }
-    })
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleCancelAdd() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
   }
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = (
+      <NewProject onAdd={handleAddProject} onCancelAdd={handleCancelAdd} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        projects={projectsState.projects}
+        onStartAddProject={handleStartAddProject}
+      />
       {content}
     </main>
   );
